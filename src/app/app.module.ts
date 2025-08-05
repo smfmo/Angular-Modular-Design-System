fb9ec9403b62973bde70479f6bad36b2382c7582
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-05 12:19:32 -0300

    Initial Commit: Advanced Modular Angular Project Initialized

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+/*
+NgModule Decorator -> Define que a classe é um módulo angular
+-> Declarations: Lista de componentes, diretivas e pipes
+que pertencem a este módulo. 
+(AppComponent é o componente principal)
+Só podem ser usados dentro deste módulo.
+
+-> imports: Aqui se importam outros módulos necessários
+
+-> providers: lista de servicos disponiveis para injeção de dependencia
+Serviços aqui ficam disponíveis para toda a aplicação.
+
+->bootstrap: Define qual componente será renderizado inicialmente.
+Normalmente o AppComponent (componente raiz)
+
+O angular cria uma instância desse componente no elemente <app-root>
+
+*/
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
