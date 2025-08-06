54358a6695401a9697cbb72b56c864c7de36191e
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-06 14:37:32 -0300

    implementação concluida, formulário do front-end está fazendo a requisição corretamente para o backend

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { CategoriaService } from '../categoria.service';
 
 @Component({
   selector: 'app-categoria',
@@ -11,7 +12,7 @@ export class CategoriaComponent {
 
   public camposForm: FormGroup;
 
-  public constructor() {
+  public constructor(private categoriaService: CategoriaService) {
     this.camposForm = new FormGroup({
       nome: new FormControl("", Validators.required),
       descricao: new FormControl("", Validators.required)
@@ -21,7 +22,15 @@ export class CategoriaComponent {
   public salvar(): void {
     this.camposForm.markAllAsTouched();
     if(this.camposForm.valid) {
-      console.log('valores digitados: ', this.camposForm.value)
+      this.categoriaService
+        .save(this.camposForm.value)
+        .subscribe({
+          next: categoria => {
+            console.log("Salvo com sucesso!", categoria);
+            this.camposForm.reset();
+          },
+          error: erro => console.error("Ocorreu um erro", erro)
+        });
     }
   }
 
