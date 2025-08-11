dd9a1e106023d561d95b691969c0cffd938cb875
Samuel Monteiro <smf.monteiro1701@gmail.com>
2025-08-11 09:48:21 -0300

    Configurando as variáveis de produção apontando a aplicação para a API que está no container docker

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,8 @@
+/**
+ * Ambiente de produção (Docker) 
+ * Apontando para o container da API.
+ */
+export const environment = {
+    production: true,
+    apiUrl: "http://descubra-lugares-application-1:8080",
+};
